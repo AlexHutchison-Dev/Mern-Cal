5efef3a5a2f25c7e2b98f79879edd955f13523ee
Alex Hutchison <a.hutchison@live.co.uk>
2021-03-07 12:10:16 -0800

    Added pProduction build

---
+++ client/build/precache-manifest.4f437007d4eb0745169fd93f97bf0fc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdae500257e413ba6c5a72ec0ef90c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "902f2015d2c4d5e732f5",
+    "url": "/static/js/2.ba344a91.chunk.js"
+  },
+  {
+    "revision": "fa6560cf6c58123b9de0a90aeddac255",
+    "url": "/static/js/2.ba344a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d55f4336fd87ce02b9",
+    "url": "/static/js/main.300d419b.chunk.js"
+  },
+  {
+    "revision": "6c6fba9d7f9451bfee27",
+    "url": "/static/js/runtime-main.211a9a59.js"
+  },
+  {
+    "revision": "9446c3e5f17ef4bc598064d4a23c36f2",
+    "url": "/static/media/Man.9446c3e5.jpg"
+  },
+  {
+    "revision": "843388096b1a1ef8fc55580c582ecda5",
+    "url": "/static/media/Woman.84338809.jpg"
+  },
+  {
+    "revision": "fb98c7aa208697e50222d1ec54c94558",
+    "url": "/static/media/title-image.fb98c7aa.png"
+  }
+]);
